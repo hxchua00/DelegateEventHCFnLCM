c63d1692e3adfbcb9145b095d3dec58eb9942798
hxchua00 <hxchua00@gmail.com>
2021-09-23 00:17:44 +0800

    Add project files.

---
+++ DelegateHCFnLCM/HCFnLCM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventHCFnLCM
+{
+    class HCFnLCM
+    {
+        int temp, a, b, x, y, HCF, LCM;
+
+        public HCFnLCM(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void findHCFnLCM(int x, int y)
+        {
+            //No change to original input
+            a = x;
+            b = y;
+
+            //Finding HCF
+            while (b != 0)
+            {
+                temp = b; //Store in temp to not lose original value
+                b = a % b; //if remainder = 0, it means HCF have been reached.
+                a = temp;
+            }
+
+            HCF = a;
+            LCM = (x * y) / HCF;
+
+            Console.WriteLine("HCF is " + HCF);
+            Console.WriteLine("LCM is " + LCM);
+        }
+    }
+}
---
+++ DelegateHCFnLCM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEventHCFnLCM
+{
+    public delegate void MathOpsEventHandler(int x, int y);
+    class Program
+    {
+        public static event MathOpsEventHandler MathOps;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int y = int.Parse(Console.ReadLine());
+
+            HCFnLCM result = new HCFnLCM(x, y);
+            MathOps += result.findHCFnLCM;
+            MathOps.Invoke(x, y);
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
